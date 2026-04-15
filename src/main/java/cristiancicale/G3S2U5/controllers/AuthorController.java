373a0e0a3c5371d4ad738ba55f629ebfc0c916fa
cicalecristian <cicalecristian@gmail.com>
2026-04-15 16:57:31 +0200

    controllers created

--- src/main/java/cristiancicale/G3S2U5/controllers/AuthorController.java
+++ src/main/java/cristiancicale/G3S2U5/controllers/AuthorController.java
@@ -1,8 +1,11 @@
 package cristiancicale.G3S2U5.controllers;
 
+import cristiancicale.G3S2U5.entities.Author;
+import cristiancicale.G3S2U5.payloads.NewAuthorPayload;
 import cristiancicale.G3S2U5.services.AuthorService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authors")
@@ -12,5 +15,40 @@ public class AuthorController {
 
     public AuthorController(AuthorService authorService) {
         this.authorService = authorService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    public Author saveAuthor(@RequestBody NewAuthorPayload body) {
+
+        return this.authorService.save(body);
+    }
+
+    // 2. GET http://localhost:3001/users
+    @GetMapping
+    public Page<Author> getAuthors(@RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "10") int size,
+                                   @RequestParam(defaultValue = "cognome") String sortBy) {
+
+        return this.authorService.findAll(page, size, sortBy);
+    }
+
+    // 3. GET http://localhost:3001/users/{userId}
+    @GetMapping("/{authorId}")
+    public Author getById(@PathVariable long authorId) {
+        return this.authorService.findById(authorId);
+    }
+
+    // 4. PUT http://localhost:3001/users/{userId} (+ req.body)
+    @PutMapping("/{authorId}")
+    public Author getByIdAndUpdate(@PathVariable long authorId, @RequestBody NewAuthorPayload body) {
+        return this.authorService.findByIdAndUpdate(authorId, body);
+    }
+
+    // 5. DELETE http://localhost:3001/users/{userId}
+    @DeleteMapping("/{authorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
+    public void getByIdAndDelete(@PathVariable long authorId) {
+        this.authorService.findByIdAndDelete(authorId);
     }
 }
--- src/main/java/cristiancicale/G3S2U5/controllers/BlogPostController.java
+++ src/main/java/cristiancicale/G3S2U5/controllers/BlogPostController.java
@@ -1,8 +1,11 @@
 package cristiancicale.G3S2U5.controllers;
 
+import cristiancicale.G3S2U5.entities.BlogPost;
+import cristiancicale.G3S2U5.payloads.NewBlogPostPayload;
 import cristiancicale.G3S2U5.services.BlogPostService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/blogPosts")
@@ -11,5 +14,36 @@ public class BlogPostController {
 
     public BlogPostController(BlogPostService blogPostService) {
         this.blogPostService = blogPostService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    public BlogPost saveBlogPost(@RequestBody NewBlogPostPayload body) {
+
+        return this.blogPostService.save(body);
+    }
+
+    @GetMapping
+    public Page<BlogPost> getUsers(@RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "10") int size,
+                                   @RequestParam(defaultValue = "categoria") String sortBy) {
+
+        return this.blogPostService.findAll(page, size, sortBy);
+    }
+
+    @GetMapping("/{blogPostId}")
+    public BlogPost getById(@PathVariable long blogPostId) {
+        return this.blogPostService.findById(blogPostId);
+    }
+
+    @PutMapping("/{blogPostId}")
+    public BlogPost getByIdAndUpdate(@PathVariable long blogPostId, @RequestBody NewBlogPostPayload body) {
+        return this.blogPostService.findByIdAndUpdate(blogPostId, body);
+    }
+
+    @DeleteMapping("/{blogPostId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
+    public void getByIdAndDelete(@PathVariable long blogPostId) {
+        this.blogPostService.findByIdAndDelete(blogPostId);
     }
 }
