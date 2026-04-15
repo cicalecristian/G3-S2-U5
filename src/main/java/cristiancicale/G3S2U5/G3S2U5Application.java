0da2bd559c44ac37f80f27cb30133b42a3b90e98
cicalecristian <cicalecristian@gmail.com>
2026-04-15 15:03:17 +0200

    Initial commit

---
+++ src/main/java/cristiancicale/G3S2U5/G3S2U5Application.java
@@ -0,0 +1,13 @@
+package cristiancicale.G3S2U5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G3S2U5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G3S2U5Application.class, args);
+	}
+
+}
